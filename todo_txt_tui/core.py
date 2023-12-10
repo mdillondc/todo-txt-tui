2d26082bfa1951b247ae218f1cb288a23017aaba
stian <stian@mandarindesign.no>
2023-12-10 16:13:48 +0100

    Version 0.2.7

--- todo_txt_tui/core.py
+++ todo_txt_tui/core.py
@@ -26,7 +26,7 @@ def is_valid_date(string):
         return False
 
 
-__version__ = '0.2.6'
+__version__ = '0.2.7'
 __package__ = 'todo-txt-tui'
 __sync_refresh_rate__ = 2
 __track_focused_task_interval__ = .1
